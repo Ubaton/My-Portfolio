24b625b55cc91c21e60f5cbe15b8e3b9464d43e0
Ubaton <raymondNG729@gmail.com>
2023-06-27 21:57:22 +0200

    v-0.1.5

--- html/js/script.js
+++ html/js/script.js
@@ -30,3 +30,34 @@ window.onscroll = () => {
 let header = document.querySelector("header");
 
 header.classList.toggle("sticky", window.scrollY > 100);
+
+menuIcon.classList.remove("bx-x");
+navbar.classList.remove("active");
+
+ScrollReveal({
+  // reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img, .services-container, .portfolio-box, .contact form",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
+ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
+
+const typed = new Typed(".multiple-text", {
+  strings: [
+    "Self-taught Full-stack Software Developer",
+    "Backend Integrator",
+    "Web Upgrades",
+    "UI/UX Designer",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
